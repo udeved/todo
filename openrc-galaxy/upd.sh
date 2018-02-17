2657a0442ad7c0c0bd00c93dbfe20cc03ffc6f73
udeved <artoo@cromnix.org>
2018-02-17 15:01:45 +0100

    finished

--- openrc-galaxy/upd.sh
+++ openrc-galaxy/upd.sh
@@ -3,8 +3,10 @@
 dirs=$(find . -mindepth 1 -maxdepth 1 -type d)
 
 for d in ${dirs[@]};do
-    cd $d
-    mkdir -v trunk
-    mv -v * trunk/
-    cd ..
+    cd $d/trunk
+        makepkg -d
+        rm -v *.pkg.tar.xz
+#     mkdir -v trunk
+#     mv -v * trunk/
+    cd ../..
 done
--- openrc-system/upd.sh
+++ openrc-system/upd.sh
@@ -2,14 +2,11 @@
 
 dirs=$(find . -mindepth 1 -maxdepth 1 -type d)
 
-# for d in ${dirs[@]};do
-#     cd $d
-#     [[ -d trunk ]] && mkdir -v trunk
-#     mv -v * trunk/
-#     cd ..
-# done
 for d in ${dirs[@]};do
     cd $d/trunk
-    rm *.pkg.tar.xz
+        makepkg -d
+        rm -v *.pkg.tar.xz
+#     mkdir -v trunk
+#     mv -v * trunk/
     cd ../..
 done
