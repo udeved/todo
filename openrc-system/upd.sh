ab0d955192db89b083ed90fdb7b88a2c80766f7d
udeved <artoo@cromnix.org>
2018-02-17 12:57:46 +0100

    done

--- openrc-system/upd.sh
+++ openrc-system/upd.sh
@@ -2,9 +2,14 @@
 
 dirs=$(find . -mindepth 1 -maxdepth 1 -type d)
 
+# for d in ${dirs[@]};do
+#     cd $d
+#     [[ -d trunk ]] && mkdir -v trunk
+#     mv -v * trunk/
+#     cd ..
+# done
 for d in ${dirs[@]};do
-    cd $d
-    [[ -d trunk ]] && mkdir -v trunk
-    mv -v * trunk/
-    cd ..
+    cd $d/trunk
+    rm *.pkg.tar.xz
+    cd ../..
 done
